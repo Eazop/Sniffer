c9e8a32356f2e562e1ef468cdb6bc427505a0224
Alec Edwards <ealec95@gmail.com>
2019-04-26 11:15:06 -0400

    updating model

--- Sniffer-App/src/app/Models/SensorReading.ts
+++ Sniffer-App/src/app/Models/SensorReading.ts
@@ -1,5 +1,17 @@
 export class SensorReading {
-    readings : [
-        1, 2, 3, 4, 5 ,6
-    ]
+    sensor_id : number;
+    sensor_name : string;
+    data_collection_time : Date;
+    pm10_standard : number;
+    pm25_standard : number;
+    pm100_standard : number;
+    pm10_env : number;
+    pm25_env : number;
+    pm100_env : number;
+    particles_03um : number;
+    particles_05um : number;
+    particles_10um : number;
+    particles_25um : number;
+    particles_50um : number;
+    particles_100um : number;
 }
