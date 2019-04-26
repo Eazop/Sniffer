2aa2e41e572fcafd6cf3c3860fcfb7c2d75bd0b9
BrianAshcroft <brianashcroft@gmail.com>
2019-04-26 11:57:29 -0400

    Create sensorModel.ts

---
+++ Sniffer-App/src/app/Models/sensorModel.ts
@@ -0,0 +1,17 @@
+export class sensorModel {
+    sensor_id: number;
+    sensor_name: string;
+    data_collection_time: Date;
+    pm10_standard: number;
+    pm25_standard: number;
+    pm100_standard: number;
+    pm10_env: number;
+    pm125env: number;
+    pm100_env: number;
+    particles_03um
+    particles_05um: number;
+    particles_10um: number;
+    particles_25um: number;
+    particles_50um: number;
+    particles_100um: number;
+}
