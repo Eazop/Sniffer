910701023d6a4340eb920b5c00375b4bb2f5167e
BrianAshcroft <brianashcroft@gmail.com>
2019-04-26 11:56:40 -0400

    Update app.component.ts

--- Sniffer-App/src/app/app.component.ts
+++ Sniffer-App/src/app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FirebaseService } from '~/app/Services/firebase/firebase.service';
+import { Subscription } from 'rxjs';
+import { sensorModel } from "./Models/data";
 
 @Component({
     selector: "ns-app",
     moduleId: module.id,
     templateUrl: "./app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit  {
+
+    constructor(private firebase: FirebaseService) { }
+
+    ngOnInit() {
+        this.firebase.getData();
+    }
+}
